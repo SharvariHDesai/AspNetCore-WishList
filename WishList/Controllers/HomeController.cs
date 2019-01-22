9949c24f2ebcb6cf9d89c09624e2ba2410ae1f6e
Sharvari <sharvaridesai@hotmail.com>
2019-01-21 20:08:35 -0500

    create

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -1,10 +1,18 @@
 ﻿using System;
-namespace WishList.Controller
+using Microsoft.AspNetCore.Mvc;
+namespace WishList.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public HomeController()
+        public IActionResult Index()
         {
+            return View("Index");
+        }
+
+
+        public IActionResult Error()
+        {
+            return View("Error");
         }
     }
 }
