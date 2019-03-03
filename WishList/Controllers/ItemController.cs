cb99fff2ddeaa4714bf9f138154de06f865eab1c
Sharvari <sharvaridesai@hotmail.com>
2019-03-03 18:41:59 -0500

    Update ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -31,11 +31,9 @@ namespace WishList.Controllers
             _context.SaveChanges();
           return  RedirectToAction("Index");
         }
-
-
         public IActionResult Delete(int Id)
         {
-            var item = _context.Items.FirstOrDefault(e => e.Id == Id);
+            var item = _context.Items.FirstOrDefault(e =>e.Id == Id);
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
